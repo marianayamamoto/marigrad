d80688b36f276e24c76b1cb749bede60ce5f2f6f
Mariana Yamamoto <mariana.mhy@gmail.com>
2014-08-25 13:31:01 -0300

    Adição de checkbox
    Com cálculo de total de créditos

--- js/controller.js
+++ js/controller.js
@@ -3,5 +3,49 @@ var app = angular.module('promari', []);
 app.controller('MainController', function ($scope,$http) {
   $http.get('periodos.json').success(function(data) {
     $scope.periodos = data;
+    $http.get('disciplinas.json').success(function(data) {
+    	$scope.disciplinas = data;
+    	angular.forEach($scope.periodos, function(value, key){
+    		var periodo = key;
+    		//console.log(value);
+    		angular.forEach($scope.periodos[periodo].disciplinas, function(value, key){
+    			var codDisciplina = value;
+    			var posDisciplina = key;
+	    		//console.log($scope.disciplinas.disciplinas);
+	    		angular.forEach($scope.disciplinas.disciplinas, function(value, key){
+    				//var codDisciplina = value;
+    				if(value.codigo == codDisciplina){
+    					$scope.periodos[periodo].disciplinas[posDisciplina] = value;
+    				}
+		    		
+		    	});	
+	    	});	
+    	});
+  	});
   });
+
+  $scope.creditosObrigatorios = 0;
+  $scope.creditosHumCompl = 0;
+  $scope.creditosProfiss = 0;
+
+  $scope.atualizaCreditos = function($event, tipo, creditos){
+  	if(tipo == "obrigatorias"){
+  		if($event.target.checked){
+	  		$scope.creditosObrigatorios += creditos;
+	  	}else
+	  		$scope.creditosObrigatorios -= creditos;
+  	}else if(tipo == "opthumanascompl"){
+  		if($event.target.checked){
+	  		$scope.creditosHumCompl += creditos;
+	  	}else
+	  		$scope.creditosHumCompl -= creditos;
+
+  	}else if(tipo == "optprofissionalizate"){
+  		if($event.target.checked){
+	  		$scope.creditosProfiss += creditos;
+	  	}else
+	  		$scope.creditosProfiss -= creditos;
+  	}
+  }
+  
 });
