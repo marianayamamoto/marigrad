7426cd2e43d3b2ae34b604b5d5c457d874104b11
Mariana Yamamoto <mariana.mhy@gmail.com>
2014-08-29 17:34:18 -0300

    Seleção e recontagem de todos os créditos
    Seleção/deseleção e recontagem de todos os créditos aparentemente sem
    erros de selecionar/deselecionar um crédito e selecionar/deselecionar
    todos.

--- js/controller.js
+++ js/controller.js
@@ -3,11 +3,12 @@ var app = angular.module('promari', []);
 app.controller('MainController', function ($scope,$http) {
   $http.get('periodos.json').success(function(data) {
     $scope.periodos = data;
-    $http.get('disciplinas.json').success(function(data) {
+    $http.get('disciplinas2.json').success(function(data) {
     	$scope.disciplinas = data;
     	angular.forEach($scope.periodos, function(value, key){
     		var periodo = key;
-    		//console.log(value);
+        $scope.periodos[periodo].toggleShow = true;
+        $scope.periodos[periodo].checkAll = false;
     		angular.forEach($scope.periodos[periodo].disciplinas, function(value, key){
     			var codDisciplina = value;
     			var posDisciplina = key;
@@ -28,24 +29,74 @@ app.controller('MainController', function ($scope,$http) {
   $scope.creditosHumCompl = 0;
   $scope.creditosProfiss = 0;
 
-  $scope.atualizaCreditos = function($event, tipo, creditos){
+  $scope.atualizaCreditos = function(el, tipo, creditos){
   	if(tipo == "obrigatorias"){
-  		if($event.target.checked){
+  		if(el.disciplina.checked){
 	  		$scope.creditosObrigatorios += creditos;
 	  	}else
 	  		$scope.creditosObrigatorios -= creditos;
   	}else if(tipo == "opthumanascompl"){
-  		if($event.target.checked){
+  		if(el){
 	  		$scope.creditosHumCompl += creditos;
 	  	}else
 	  		$scope.creditosHumCompl -= creditos;
 
-  	}else if(tipo == "optprofissionalizate"){
-  		if($event.target.checked){
+  	}else if(tipo == "optprofissionalizante"){
+  		if(el){
 	  		$scope.creditosProfiss += creditos;
 	  	}else
 	  		$scope.creditosProfiss -= creditos;
   	}
   }
-  
+
+  $scope.recalcularCreditos = function($event,creditos){
+    var checked = $event.target.checked;
+    var elemClass = angular.element($event.target)[0].className;
+    if(elemClass.indexOf("cbperfil") > -1){
+      var children = angular.element($event.srcElement.parentElement.lastElementChild.children);
+      angular.forEach(children,function(value,key){
+        var child = angular.element(value.children);
+        var childChecked = child[0].checked;
+        var childClass = child[0].className;
+        var childCredits = parseInt(child[0].dataset.creditos);
+        if(checked){
+          if(!childChecked){
+            if(childClass.indexOf("obrigatorias") > -1){
+              $scope.creditosObrigatorios += childCredits;
+            }else if(childClass.indexOf("opthumanascompl") > -1){
+              $scope.creditosHumCompl += childCredits;
+            }else if(childClass.indexOf("optprofissionalizante") > -1){
+              $scope.creditosProfiss += childCredits;
+            }
+          }
+        }else{
+          if(childChecked){
+            if(childClass.indexOf("obrigatorias") > -1){
+              $scope.creditosObrigatorios -= childCredits;
+            }else if(childClass.indexOf("opthumanascompl") > -1){
+              $scope.creditosHumCompl -= childCredits;
+            }else if(childClass.indexOf("optprofissionalizante") > -1){
+              $scope.creditosProfiss -= childCredits;
+            }
+          }
+        }
+      });
+    }else if(elemClass.indexOf("obrigatorias") > -1){
+      if(checked){
+        $scope.creditosObrigatorios += creditos;
+      }else
+        $scope.creditosObrigatorios -= creditos;
+    }else if(elemClass.indexOf("opthumanascompl") > -1){
+      if(checked){
+        $scope.creditosHumCompl += creditos;
+      }else
+        $scope.creditosHumCompl -= creditos;
+
+    }else if(elemClass.indexOf("optprofissionalizante") > -1){
+      if(checked){
+        $scope.creditosProfiss += creditos;
+      }else
+        $scope.creditosProfiss -= creditos;
+    }
+  }
 });
